42ba87b4176ca5f5834411afb52ebd3bddcca45f
Starinin Andrey <starininandrey@gmail.com>
2022-09-01 17:31:20 +0300

    Init

---
+++ BindingDemo.App/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace BindingDemo.App;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private string _name;
+
+    public string FirstName
+    {
+        get => _name;
+        set
+        {
+            if (value == _name)
+            {
+                return;
+            }
+
+            _name = value;
+            OnPropertyChanged(nameof(FirstName));
+        }
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonExport_OnClick(object sender, RoutedEventArgs e)
+    {
+        using var file = new StreamWriter("name.dat", false);
+        file.WriteLine(FirstName);
+    }
+
+    private void ButtonImport_OnClick(object sender, RoutedEventArgs e)
+    {
+        using var file = new StreamReader("name.dat");
+        FirstName = file.ReadLine();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
